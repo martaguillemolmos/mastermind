cd92dd972af93a63f83c9c5a8f41c547ec8ffd62
martaguillemolmos <martetaguillem@gmail.com>
2023-10-10 14:04:58 +0200

    Damos formato a la ventana de la pestaña

--- js/loser.js
+++ js/loser.js
@@ -1,6 +1,6 @@
 let nombre = localStorage.getItem('name');
 let contenedorNombre = document.getElementById('contenedorNombre');
-contenedorNombre.textContent = nombre
+contenedorNombre.textContent = nombre;
 
 let reset = document.getElementById('botonResetear');
 reset.addEventListener("click", () => {
